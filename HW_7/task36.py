2dc940409d5e303d0d5851675219182eef1b407f
zoya-ivanova <116255881+zoya-ivanova@users.noreply.github.com>
2023-03-12 13:56:34 +0500

    Update HW_7/task36.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- HW_7/task36.py
+++ HW_7/task36.py
@@ -16,6 +16,6 @@
 def print_operation_table(operation, num_rows = 6, num_columns = 6):
     for i in range(1, num_rows + 1):
         for j in range(1, num_columns + 1):
-            print(*list(map(operation, [i], [j])), end="\t")    # \t(tab) табуляция (отступ, красная строка)
+            print(*map(operation, [i], [j]), end="\t")  # \t(tab) табуляция (отступ, красная строка)
         print()
 print_operation_table(lambda x, y: x * y) 	
